dc85e32c70f14bd48869fb037379d7638a345fc2
ali <mislombek27@gmail.com>
2020-05-05 11:38:21 +0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App.vue'
+import response from "vue-resource/src/http/response";
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vuejs-http-68207.firebaseio.com/'
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if(request.method == 'POST'){
+    request.method = 'PUT'
+  }
+  next(response => {
+    response.json = () => {return {message: response.body}}
+  });
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
